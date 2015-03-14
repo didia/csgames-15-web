f9847bd362930ca1facd313a0aed0eb37244ed39
Aristote Diasonama <aristotediasonama@yahoo.fr>
2015-03-14 15:50:30 -0400

    added secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+	attr_accessor :password, :email, :password_confirmation
+	has_secure_password
+	validates :name, :presence => true
+	validates :email, :presence => true, :uniqueness => true
+	validates_presence_of :password, :on => :create
+	validates :age, :presence => true
+	validates :gender, :presence => true
+	validates_length_of :password, :in => 6..20, :on => :create
+
 end
