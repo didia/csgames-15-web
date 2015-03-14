44677dc1e3c1d11fe2478d4307400678aa508090
Aristote Diasonama <aristotediasonama@yahoo.fr>
2015-03-14 16:17:15 -0400

    Corrected the sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
   		user = User.find_by_email(params[:email])
   		if user && user.authenticate(params[:password])
     		session[:user_id] = user.id
-   	 		redirect_to user, :notice => "Welcome back, #{user.name}"
+   	 		redirect_to user_url(user), :notice => "Welcome back, #{user.name}"
   		else
     		flash.now.alert = "Invalid email or password"
     		render "new"  
     	end
+    end
 
   end
 
   def delete
+  	reset_session
+  	redirect_to root_path
   end
 end
